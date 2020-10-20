8a15883bd9d4f2dd477672f3c91e59525a671799
Charlie <jinze@athinkingape.com>
2020-10-20 11:55:39 -0700

    test

---
+++ commit-msg.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python
+
+import sys
+
+def main()
+	print('Number of arugments: %d', len(sys.argv))
+	print('Argument List: %s' % str(sys.argv))
+
+	sys.exit(1)
+
+if __name__ == "__main__":
+	main()
