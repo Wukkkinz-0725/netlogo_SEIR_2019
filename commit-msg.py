f83c34bf89aaae96621072dc34f096c0ab5a2996
Charlie <jinze@athinkingape.com>
2020-10-20 12:02:01 -0700

    test1

--- commit-msg.py
+++ commit-msg.py
@@ -5,7 +5,7 @@ import sys
 def main()
 	print('Number of arugments: %d', len(sys.argv))
 	print('Argument List: %s' % str(sys.argv))
-
+	print('cyzpiggy')
 	sys.exit(1)
 
 if __name__ == "__main__":
