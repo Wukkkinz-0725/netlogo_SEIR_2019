4c6aa38a49be9ad5751ce562804ea9b1e226fb0f
Charlie <jinze@athinkingape.com>
2020-10-22 03:15:28 -0700

    a

--- git-hook/pre-commit.py
+++ git-hook/pre-commit.py
@@ -7,18 +7,19 @@ def main():
 	cmd = 'git diff --name-only --cached'
 	result = subprocess.check_output(cmd, shell=True)
 	files = result.split('\n')
+	print('aaaa')
 	print(files)
-	for file in files:
-		# print(file)
-		if file.endswith('py'):
-			print(file)
-			with open(file, 'r') as read_file:
-				tmp = read_file.read()
-				print(tmp)
+
+	# for file in files:
+	# 	if file.endswith('py'):
+	# 		print(file)
+	# 		with open(file, 'r') as read_file:
+	# 			tmp = read_file.read()
+	# 			print(tmp)
 	# print('AAAAAAAAA: %d', len(sys.argv))
 	# print('BBBBBBBBB: %s' % str(sys.argv))
 	# print('CCCCCCCCD')
-	sys.exit(1)
+	sys.exit(0)
 
 if __name__ == "__main__":
 	main()
