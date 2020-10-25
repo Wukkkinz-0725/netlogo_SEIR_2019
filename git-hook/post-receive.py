06caf996b2c949f0a984abb2bde211b84b41afce
Charlie <jinze@athinkingape.com>
2020-10-25 01:27:14 -0700

    a

--- git-hook/post-receive.py
+++ git-hook/post-receive.py
@@ -5,7 +5,10 @@ import sys
 import subprocess
 
 def main():
+
 	print('welcome to post-receive!')
+	with open('/Users/jinze/Desktop/abc.txt', 'w') as output_file:
+		output_file.write('abc') 
 	sys.exit(0)
 
 if __name__ == "__main__":
