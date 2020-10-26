ddc1061de846a6959072d32aa6751c7909bf3040
Charlie <jinze@athinkingape.com>
2020-10-25 20:21:20 -0700

    abc

--- git-hook/post-receive.py
+++ git-hook/post-receive.py
@@ -4,12 +4,5 @@ import git
 import sys
 import subprocess
 
-def main():
-	abcabc
-	print('welcome to post-receive!')
-	with open('/Users/jinze/Desktop/abc.txt', 'w') as output_file:
-		output_file.write('abc') 
-	sys.exit(1)
-
-if __name__ == "__main__":
-	main()
+oldrev,newrev,refname = sys.stdin.readline().strip().split(' ')
+print('oldref:%s, newrev:%s, refname:%s'%(oldrev, newrev, refname))
