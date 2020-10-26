3a7167a4945d541136ec2b85d2bed390d6eaccb0
Charlie <jinze@athinkingape.com>
2020-10-26 09:51:27 -0700

    bcd

--- git-hook/post-receive.py
+++ git-hook/post-receive.py
@@ -4,5 +4,6 @@ import git
 import sys
 import subprocess
 print('cyzpiggy')
+print('bcd')
 oldrev,newrev,refname = sys.stdin.readline().strip().split(' ')
 print('oldref:%s, newrev:%s, refname:%s'%(oldrev, newrev, refname))
